dad8c48b3630d1564224571ef181aaa5baf022fc
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2024-02-25 15:20:46 +0100

    feat: add packages to index

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const http = require('http');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require ('dotenv').config();
+const port = 3000;
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+/*mongoose.connect(process.env.MONGODB);
+console.log(process.env.MONGODB);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function () {
+  console.log("Connected to MongoDB");
+});*/
+
+const server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
