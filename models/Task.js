7bdfdbe85dcdc2a176972a708d4f4aeed81eaf2e
Yadina Moreira Cunha Filho <r0784273@student.thomasmore.be>
2024-02-27 20:13:08 +0100

    feat: add task model

---
+++ models/Task.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    timeMorning: {
+        type: String,
+        required: true,
+    },
+    timeAfternoon: {
+        type: String,
+        required: true,
+    },
+    timeEvening: {
+        type: String,
+        required: true,
+    },
+    completed: {
+        type: Boolean,
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    allDays: {
+        type: Boolean,
+        required: true,
+    },
+});
+
+const Task = mongoose.model('Task', taskSchema);
+module.exports = Task;
