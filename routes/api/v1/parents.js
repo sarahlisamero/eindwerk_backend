8bf8aa10f59ca602749fde0a01d28448c86b1481
zzahraJ <r0883814@student.thomasmore.be>
2024-02-28 21:32:47 +0100

    feat: add signup and login route

--- routes/api/v1/parents.js
+++ routes/api/v1/parents.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const parentsController = require('../../../controllers/api/v1/parents');
+const authController = require('../../../controllers/api/v1/auth');
 
 router.get('/', parentsController.getAllParents);
 router.get('/:id', parentsController.getParentById);
 router.post('/', parentsController.createParent);
+
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
 
 module.exports = router;
