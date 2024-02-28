79d72b8edc57009d4f60022b3237be2fc51ed97c
Yadina Moreira Cunha Filho <r0784273@student.thomasmore.be>
2024-02-28 18:12:28 +0100

    feat: add get task by id

--- controllers/api/v1/tasks.js
+++ controllers/api/v1/tasks.js
@@ -23,4 +23,17 @@ const createTask = async (req, res) => {
     }
 };
 
+const getTaskById = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+        if (!task) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+        res.json(task);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
 module.exports.createTask = createTask;
+module.exports.getTaskById = getTaskById;
--- routes/api/v1/tasks.js
+++ routes/api/v1/tasks.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const tasksController = require('../../../controllers/api/v1/tasks');
 
 router.post('/', tasksController.createTask);
+router.get('/:id', tasksController.getTaskById);
 
 module.exports = router;
