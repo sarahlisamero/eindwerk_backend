b6f67abde53c8c87af0e62e1b10cd4d294af2d7b
zzahraJ <r0883814@student.thomasmore.be>
2024-03-02 14:41:52 +0100

    feat: add admin, children

--- models/Parent.js
+++ models/Parent.js
@@ -2,31 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
-const Parent = new Schema({});
+const Parent = new Schema({
+    admin: {
+        type: Boolean,
+        default: true,
+    },
+    children: [{ 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'Child' 
+    }] //many to many relationship
+});
 
 Parent.plugin(passportLocalMongoose);
 
-
-// const parentSchema = new Schema({
-//     username: {
-//         type: String,
-//         required: true,
-//         unique: true, //no double usernames
-//     },
-//     password: {
-//         type: String,
-//         required: true,
-//     },
-//     admin: {
-//         type: Boolean,
-//         default: true,
-//     },
-//     children: [{ 
-//         type: mongoose.Schema.Types.ObjectId, 
-//         ref: 'Child' 
-//     }] //many to many relationship
-// });
-
-// const Parent = mongoose.model('Parent', parentSchema);
-// module.exports = Parent;
 module.exports = mongoose.model ('Parent', Parent)
