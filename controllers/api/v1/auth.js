64de0b990ef2781f493d27b28dff433ce01929ac
zzahraJ <r0883814@student.thomasmore.be>
2024-03-02 14:41:36 +0100

    feat: add admin, children

--- controllers/api/v1/auth.js
+++ controllers/api/v1/auth.js
@@ -1,13 +1,16 @@
 const Parent = require('../../../models/Parent');
 const jwt = require('jsonwebtoken'); 
-// const passport = require('../../../passport/passport');
+//const config = require('config');
+const { use } = require('passport');
 
 const signup = async (req, res) => {
     let username = req.body.username;
     let password = req.body.password;
+    let admin = req.body.admin;
+    let children = req.body.children;
 
     const parent = new Parent({ 
-        username: username 
+        username: username, admin: admin, children: children 
     });
 
     await parent.setPassword(password);
@@ -31,46 +34,27 @@ const signup = async (req, res) => {
     });
 };
 
-// const login = async (req, res, next) => {
-//     const parent = await Parent.authenticate()(req.body.username, req.body.password).then(result => {
-       
-//         if (!result.parent) {
-//             return res.json({
-//                 "status": "failed",
-//                 "message": "Login failed"
-//             })
-//         }
-
-//         let token = jwt.sign({
-//             uid: result.user._id,
-//             username: result.user.username
-//         }, process.env.JWT_SECRET);
-
-//         return res.json({
-//             "status": "success",
-//             "data": {
-//                 "token": token
-//             }
-//         });
-//     }).catch(error => {
-//         res.json({
-//             "status": "error",
-//             "message": error
-//         })
-//     });
-// };
-
-
 const login = async (req, res, next) => {
-    const parent = await Parent.authenticate()(req.body.username, req.body.password).then(result => {
-    
-    res.json({
-        "status": "You are logged in",
-        "data": {
-            "parent": result
+    const user = await Parent.authenticate()(req.body.username, req.body.password).then(result => {
+       
+        if (!result.user) {
+            return res.json({
+                "status": "failed",
+                "message": "Login failed"
+            })
         }
-    });
 
+        let token = jwt.sign({
+            uid: result.user._id,
+            username: result.user.username
+        }, process.env.JWT_SECRET);
+
+        return res.json({
+            "status": "success",
+            "data": {
+                "token": token
+            }
+        });
     }).catch(error => {
         res.json({
             "status": "error",
