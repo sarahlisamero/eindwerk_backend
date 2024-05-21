6e8f80e4eeb572ab20f4c0a339cc1d4dd45139ee
zzahraJ <r0883814@student.thomasmore.be>
2024-05-21 15:11:24 +0200

    feat: add upload documents

--- models/Child.js
+++ models/Child.js
@@ -23,7 +23,14 @@ const childSchema = new Schema({
         default: 'https://albanyvet.com.au/wp-content/uploads/2019/11/blank-profile-picture-973460_640.png',
     },
     document: [{
-        type: String,
+        url: {
+            type: String,
+            required: true
+        },
+        public_id: {
+            type: String,
+            required: true
+        }
     }],
     avatar: {
         type: String,
